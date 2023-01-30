a3d108e31dca359d7a558cb67c114649861cdacd
William A. Strader <William.A.Strader@saic.com>
2023-01-30 15:12:31 -0500

    Base Setup

---
+++ modules/ai_drivers/functions/CfgFunctions.hpp
@@ -0,0 +1,10 @@
+class COMPONENT {
+	tag = COMPONENT;
+    class AIDrivers {
+    	file = "modules\ai_drivers\functions\AIDrivers";
+    	class createDriver {};
+        class enableAIDriver {};
+    	class removeDriver {};
+    	class toggleDriverCam {};
+    };
+};
